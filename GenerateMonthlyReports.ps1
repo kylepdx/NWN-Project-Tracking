e11aec636c406bed47b1e3f13938e8347ab40bf7
Kyle <kyle@pdxportals.com>
2019-01-30 17:12:44 -0800

    js for Monthly Status date dropdown

---
+++ GenerateMonthlyReports.ps1
@@ -0,0 +1,95 @@
+﻿# 
+#  Script to populate Project Status for a Month 
+# 
+
+
+$siteUrl = 'http://firefly.pdx.local/sites/nwnb' 
+
+
+connect-pnponline $siteUrl 
+
+$ProjectList = get-pnpList -Identity "Lists/ProjectTracking"
+$ReportDates = get-pnpList -Identity "Lists/ReportDates"
+$ProjectStatus = get-pnpList -Identity "Lists/ProjectStatus"
+
+
+
+$projects = Get-PnPListItem -List $projectList   
+$date= Get-PnPListItem -List $ReportDates -Query "<View><Query><OrderBy><FieldRef Name='StartDate' Ascending='False' /></OrderBy></Query><RowLimit Paged='False'>1</RowLimit></View>"  
+
+
+
+
+foreach ($p in $projects) 
+{
+
+    # Check if we already have a status record for this project/report date 
+    write-host "Check for " + $p.FieldValues["Title"]  
+
+    $dateLookup = "" + $date.id + ";#" + $date.FieldValues["Title"] 
+    
+
+    $query = "<View><Query><Where><And><Eq><FieldRef Name='ReportPeriod'/><Value Type='Lookup'>" + $date.FieldValues["Title"] + "</Value></Eq><Eq><FieldRef Name='Project'/><Value Type='Lookup'>" + $p.FieldValues["Title"] + "</Value></Eq></And></Where></Query></View>" 
+    $check = Get-PnPListItem -List $ProjectStatus -Query $query 
+    
+
+    if ($check -ne $null) 
+    {
+        write-host $p.FieldValues["Title"]  " already exists with " $date.FieldValues["Title"]
+
+        continue; 
+    } 
+
+
+
+    #
+    #  Retrieve the Previous Month's Data 
+    # 
+
+    $prevDate = get-date($date.FieldValues["StartDate"]).AddDays(-1) 
+    $isoDate = [microsoft.sharepoint.utilities.sputility]::CreateISO8601DateTimeFromSystemDateTime($prevDate) 
+    
+    $q = "<View><Query><Where><Lt><FieldRef Name='StartDate'/><Value Type='Datetime' IncludeTimeValue='True'>" + $isoDate + "</Value></Lt></Where><OrderBy><FieldRef Name='StartDate' Ascending='False' /></OrderBy></Query><RowLimit Paged='False'>1</RowLimit></View>"    
+          
+    $lastMonthReportPeriod = Get-PnPListItem -List $ReportDates -Query $q 
+
+  
+    $lastMonthQuery = "<View><Query><Where><And><Eq><FieldRef Name='ReportPeriod'/><Value Type='Lookup'>" + $lastMonthReportPeriod.FieldValues["Title"] + "</Value></Eq><Eq><FieldRef Name='Project'/><Value Type='Lookup'>" + $p.FieldValues["Title"] + "</Value></Eq></And></Where></Query></View>" 
+    $lastMonthReport =  Get-PnPListItem -List $ProjectStatus -Query $lastMonthQuery 
+    
+    
+    # Create a new Entry in Project Status 
+     $return = Add-PnPListItem -List $ProjectStatus  -Values @{"Title"=$p.FieldValues["Title"]}  
+     $null = Set-PnPListItem -List $ProjectStatus -Identity $return.Id   -Values @{"Project" = $p.id} 
+     $null = Set-PnPListItem -List $ProjectStatus -Identity $return.Id   -Values @{"ReportPeriod" = $date.id} 
+
+
+     if ($lastMonthReport -ne $null) 
+     {
+        $null = Set-PnPListItem -List $ProjectStatus -Identity $return.Id   -Values @{"ApprovalStatus" =  $lastMonthReport.FieldValues["ApprovalStatus"]}
+        $null = Set-PnPListItem -List $ProjectStatus -Identity $return.Id   -Values @{"GeneralComment"  = $lastMonthReport.FieldValues["GeneralComment"]}
+        $null = Set-PnPListItem -List $ProjectStatus -Identity $return.Id   -Values @{"Decisions"  = $lastMonthReport.FieldValues["Decisions"]}
+        $null = Set-PnPListItem -List $ProjectStatus -Identity $return.Id   -Values @{"Issues"  = $lastMonthReport.FieldValues["Issues"]}
+        $null = Set-PnPListItem -List $ProjectStatus -Identity $return.Id   -Values @{"Accomplishments"  = $lastMonthReport.FieldValues["Accomplishments"]}
+        $null = Set-PnPListItem -List $ProjectStatus -Identity $return.Id   -Values @{"NextSteps" = $lastMonthReport.FieldValues["NextSteps"]}
+        $null = Set-PnPListItem -List $ProjectStatus -Identity $return.Id   -Values @{"TotalBudget"  = $lastMonthReport.FieldValues["TotalBudget"]}
+        $null = Set-PnPListItem -List $ProjectStatus -Identity $return.Id   -Values @{"ActualBudget"  = $lastMonthReport.FieldValues["ActualBudget"]}
+        $null = Set-PnPListItem -List $ProjectStatus -Identity $return.Id   -Values @{"PctBudgetUsed"  = $lastMonthReport.FieldValues["PctBudgetUsed"]}
+        $null = Set-PnPListItem -List $ProjectStatus -Identity $return.Id   -Values @{"ForecastAndActuals"  = $lastMonthReport.FieldValues["ForecastAndActuals"]}
+     }
+
+     break; 
+
+     
+
+}
+
+
+# Get the newest ReportDate 
+
+
+
+
+
+
+
---
+++ jsLink/status.js
@@ -0,0 +1,76 @@
+﻿<script type="text/javascript" src="/sites/nwnb/siteassets/jslink/jquery.min.js"></script>
+<script type="text/javascript" src="/sites/nwnb/siteassets/jslink/jquery.SPServices.min.js"></script>
+
+
+<select id="ddReportDate"></select> 
+
+
+<script language="javascript" type="text/javascript">
+
+var currentDate = null; 
+
+function getQueryString ()
+{
+	var queryStringKeyValue = window.parent.location.search.replace('?', '').split('&');
+    var qsJsonObject = {};
+    if (queryStringKeyValue != '') {
+        for (i = 0; i < queryStringKeyValue.length; i++) {
+            qsJsonObject[queryStringKeyValue[i].split('=')[0]] = queryStringKeyValue[i].split('=')[1];
+        }
+    }
+    return qsJsonObject;
+}
+
+$(document).ready(function() {
+    debugger; 
+	
+	 $("#ddReportDate").change(function(){
+         var selected = $(this).val(); 
+         alert(selected); 
+         var newPage = location.href.split('?')[0] + '?date=' + encodeURIComponent(selected);
+         document.location.href = newPage; 
+     });
+
+
+	var qsDate = getQueryString().date; 
+	if (qsDate) 
+	{
+		currentDate = decodeURIComponent(qsDate); 
+	}	
+	
+	 	
+	
+  $().SPServices({
+    operation: "GetListItems",
+    async: false,
+    listName: "Report Dates",
+    CAMLViewFields: "<ViewFields><FieldRef Name='Title' /></ViewFields>",
+    CAMLQuery: "<Query><OrderBy><FieldRef Name='StartDate' Ascending='False' /></OrderBy></Query>",
+    completefunc: function (xData, Status) { 
+    
+      var select = document.getElementById("ddReportDate"); 
+     
+      $(xData.responseXML).SPFilterNode("z:row").each(function() {
+        var liHtml = "<li>" + $(this).attr("ows_Title") + "</li>";
+        $("#tasksUL").append(liHtml);
+        
+        var el = document.createElement("option");
+	    el.textContent = $(this).attr("ows_Title");
+	    el.value = $(this).attr("ows_Title"); 
+	    
+	    if (currentDate && currentDate == el.value)
+	    {
+	    	el.selected = true; 
+	    }
+	    
+	    select.appendChild(el);
+        
+      }); 
+    }
+  });
+});
+
+
+</script>
+
+<ul id="tasksUL"/>
